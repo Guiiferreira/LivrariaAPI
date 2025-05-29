34d0bcfdc8d1cc84d2343db1f7c515789d08b35c
Guilherme Ferreira <guilhermepereiraferreira4@gmail.com>
2025-05-29 18:21:10 -0300

    criando projeto e Criando e rodando os scripts no banco de dados

---
+++ src/test/java/com/guilhermeferreira/livrariaapi/LivrariaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilhermeferreira.livrariaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrariaapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
