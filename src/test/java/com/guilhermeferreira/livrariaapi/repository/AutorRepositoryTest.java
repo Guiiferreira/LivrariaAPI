b606eae66fc3d3978947950ac5e4971fe193057e
Guilherme ferreira <guilhermepereiraferreira4@gmail.com>
2025-06-18 00:17:39 -0300

    Operações basicas para entidades simples

--- src/test/java/com/guilhermeferreira/livrariaapi/repository/AutorRepositoryTest.java
+++ src/test/java/com/guilhermeferreira/livrariaapi/repository/AutorRepositoryTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -29,7 +30,7 @@ public class AutorRepositoryTest {
 
     @Test
     public void atualizarTeste() {
-        var id = UUID.fromString("771a1c14-95b9-4955-9300-ea05019921d8");
+        var id = UUID.fromString("15a7c5df-1e10-4d1b-bd42-366d40e39d8f");
         Optional<Autor> possivelAutor = repository.findById(id);
 
         if (possivelAutor.isPresent()) {
@@ -37,12 +38,35 @@ public class AutorRepositoryTest {
             System.out.println("Dados do Autor:");
             System.out.println(autorEncontrado);
 
-            autorEncontrado.setDataNascimento(LocalDate.of(1960,1,30));
+            autorEncontrado.setDataNascimento(LocalDate.of(1960, 1, 30));
 
             repository.save(autorEncontrado);
 
+            }
+
+        }
+        @Test
+        public void listarTest(){
+            List<Autor> lista = repository.findAll();
+            lista.forEach(System.out::println);
 
         }
 
+        @Test
+        public void countTest(){
+            System.out.println("Contagem de autores: " + repository.count() );
+        }
+
+        @Test
+        public void deletePorIdTest(){
+            var id = UUID.fromString("771a1c14-95b9-4955-9300-ea05019921d8");
+            repository.deleteById(id);
+        }
+         @Test
+        public void deleteTest(){
+        var id = UUID.fromString("c0688567-cf71-4e0a-9b5f-904df3fa2ff9");
+        var maria = repository.findById(id).get();
+        repository.delete(maria);
     }
-}
+
+    }
