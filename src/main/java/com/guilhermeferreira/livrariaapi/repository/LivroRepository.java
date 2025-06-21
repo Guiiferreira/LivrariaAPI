b64d70b8c5a5e28bf575cc5211484b4b98c12283
Guilherme ferreira <guilhermepereiraferreira4@gmail.com>
2025-06-20 23:40:42 -0300

    Variações de métodos de busca declarativos

--- src/main/java/com/guilhermeferreira/livrariaapi/repository/LivroRepository.java
+++ src/main/java/com/guilhermeferreira/livrariaapi/repository/LivroRepository.java
@@ -5,6 +5,7 @@ import com.guilhermeferreira.livrariaapi.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
@@ -26,4 +27,10 @@ public interface LivroRepository extends JpaRepository<Livro, UUID> {
 
     //select * from where titulo =? or isbn = ?
     List<Livro> findByTituloOrIsbn(String titulo, String isbn);
+
+    // select * from livro where data_publicacao btween ? and ?
+    List<Livro> findByDataPublicacaoBetween(LocalDate inicio, LocalDate fim);
+
+
+
 }
